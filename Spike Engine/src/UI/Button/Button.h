37c7308422c5437449ee52b12b0c8ecc1b5bebca
Örs Mándli <mandliors@gmail.com>
2020-08-20 23:52:29 +0200

    Widgets

--- Spike Engine/src/UI/Button/Button.h
+++ Spike Engine/src/UI/Button/Button.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "UI/Widget/Widget.h"
-#include "UI/Constraints/Constraint.h"
 #include <string>
 
 namespace Spike {
@@ -10,7 +9,7 @@ namespace Spike {
 	{
 	public:
 		Button() = delete;
-		Button(const std::string& text, void(*func)() = nullptr, const Color& backColor = Color(0, 128, 255, 255), const Color& borderColor = Color::White(), uint32_t borderWidt = 1, uint8_t roundness = 0);
+		Button(const std::string& text, void(*func)() = nullptr, const Color& backColor = Color(0, 128, 255, 255), const Color& borderColor = Color::White(), uint32_t borderWidth = 1, uint8_t roundness = 0);
 		~Button();
 
 		void OnClick() override;
